ec450470de6150b1528952e0c2f27abe70136a50
semio <prairy.long@gmail.com>
2018-03-27 16:08:06 +0800

    update scripts according to source dataset changes

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -3,39 +3,24 @@
 import os.path as osp
 import pandas as pd
 
-source_file = '../source/gdppc_cppp/gdppc_cppp-by-gapminder.xlsx'
+
+code = 'gdppc_cppp'
+measure_name = 'GDP per capita, constant PPP'
+
 out_path = '../../'
+
+source_file = f'../source/{code}/{code}-by-gapminder.xlsx'
+mappings_file = 'https://github.com/Gapminder-Indicators/concepts/blob/master/concepts-in-gapminder-indicators.xlsx?raw=true'
 
 
 def main():
-    code = 'gdp_per_capita_cppp'
+    print('loading mapping file...')
+    mappings = pd.read_excel(mappings_file)
 
-    # laod the about tab and extract the metadata
-    about = pd.read_excel(source_file, header=None)
-    # columns are 0: name, 1: description, 2: code
-    about = about.dropna(subset=[2], how='all')
-    about = about.set_index(2)
+    print('running etl...')
+    measure_id = mappings.set_index('repo_id')['concept'].loc[code]
 
-    # info about the indicator
-    measure = about[[1]].T
-    measure['code'] = code
-
-    measure = measure.set_index('code')
-
-    measure.index.name = 'concept'
-    measure.columns.name = None
-
-    measure['concept_type'] = 'measure'
-
-    # creata concepts DataFrame
-    concepts = about[[0]].copy()
-    concepts.columns = ['description']
-    concepts.index.name = 'concept'
-
-    concepts['description'] = concepts['description'].map(lambda x: x.replace(':', ''))
-    concepts['concept_type'] = 'string'
-
-    # set geo concepts manually.
+    # set concepts manually.
     domain = [ {'concept': 'geo', 'name': 'Geo', 'domain': None, 'concept_type': 'entity_domain'},
                {'concept': 'country', 'name': 'Country', 'domain': 'geo', 'concept_type': 'entity_set'},
                {'concept': 'world_4region', 'name': 'four regions', 'domain': 'geo', 'concept_type': 'entity_set'},
@@ -49,7 +34,13 @@ def main():
     time = pd.DataFrame.from_records([
         {'concept': 'year', 'concept_type': 'time', 'name': 'Year'}]).set_index('concept')
 
-    cdf = pd.concat([measure, concepts, domain, time])
+    strings = pd.DataFrame.from_records([
+        {'concept': 'name', 'concept_type': 'string', 'name': 'Name'}]).set_index('concept')
+
+    measure = pd.DataFrame.from_records([
+        {'concept': measure_id, 'concept_type': 'measure', 'name': measure_name}]).set_index('concept')
+
+    cdf = pd.concat([measure, strings, domain, time])
     cdf = cdf.reset_index()
 
     cdf['name'] = cdf['name'].fillna(cdf['concept'].map(lambda x: x.replace('_', ' ').title()))
@@ -66,11 +57,11 @@ def main():
 
     country_df = country_data.set_index('geo').drop(['geo.name', 'indicator', 'indicator.name'], axis=1)
     country_df = country_df.stack().reset_index()
-    country_df.columns = ['country', 'year', code]
+    country_df.columns = ['country', 'year', measure_id]
 
     country_df.dropna(how='any').to_csv(
         osp.join(out_path,
-                 'ddf--datapoints--{}--by--country--year.csv'.format(code)), index=False)
+                 'ddf--datapoints--{}--by--country--year.csv'.format(measure_id)), index=False)
 
     # region data
     region_data = pd.read_excel(source_file,
@@ -84,12 +75,12 @@ def main():
 
     region_df = region_data.set_index('geo').drop(['geo.name', 'indicator', 'indicator.name'], axis=1)
     region_df = region_df.stack().reset_index()
-    region_df.columns = ['world_4region', 'year', code]
+    region_df.columns = ['world_4region', 'year', measure_id]
 
     region_df.dropna(how='any').to_csv(
         osp.join(
             out_path,
-            'ddf--datapoints--{}--by--world_4region--year.csv').format(code), index=False)
+            'ddf--datapoints--{}--by--world_4region--year.csv').format(measure_id), index=False)
 
     # world data
     world_data = pd.read_excel(source_file,
@@ -102,11 +93,11 @@ def main():
 
     world_df = world_data.set_index('geo').drop(['geo.name', 'indicator', 'indicator.name'], axis=1)
     world_df = world_df.stack().reset_index()
-    world_df.columns = ['global', 'year', code]
+    world_df.columns = ['global', 'year', measure_id]
 
     world_df.dropna(how='any').to_csv(
         osp.join(out_path,
-                 'ddf--datapoints--{}--by--global--year.csv').format(code), index=False)
+                 'ddf--datapoints--{}--by--global--year.csv').format(measure_id), index=False)
 
 
 if __name__ == '__main__':
